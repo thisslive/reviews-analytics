72b0f912ef86eae7e2f51496be156cca3f658b84
Ou Frank <oufrank@Oude-MacBook-Pro.local>
2020-03-24 12:30:15 +0800

    final

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -15,3 +15,19 @@ for d in data:
 	sum_len += len(d)
 
 print('每筆留言的平均長度為',sum_len/len(data))
+
+
+new=[]
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'筆長度小於100')
+print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有',len(good),'筆提到good')
+print(good[0])
+
